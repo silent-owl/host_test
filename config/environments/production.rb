d470c01228e33766887fe77243c55134484824ff
Silent Owl <nikolas.net.mail@gmail.com>
2018-07-31 21:06:24 +0300

    Add fix prod email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,8 +8,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app104419507@heroku.com'],
-    :password       => ENV['rcvagtrp7711'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
